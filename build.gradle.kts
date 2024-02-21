23f928ef2b9d4b95716baecc9781c6b9f779f938
night <nightfeng26@gmail.com>
2024-02-21 17:21:18 +0800

    serialize task

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,8 @@ extensions.findByName("buildScan")?.withGroovyBuilder {
     setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
     setProperty("termsOfServiceAgree", "yes")
 }
+
+tasks.register("clean", Delete::class.java) {
+    delete(rootProject.buildDir)
+    dependsOn(gradle.includedBuilds.map { it.task(":clean") })
+}
