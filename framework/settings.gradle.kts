4084f98bf9cab718371341a178e692171b87e59e
night <nightfeng26@gmail.com>
2024-02-26 20:24:42 +0800

    replace with symbolic link

--- framework/settings.gradle.kts
+++ framework/settings.gradle.kts
@@ -8,7 +8,9 @@ pluginManagement {
 
 if (gradle.parent == null) {
     val initScriptFile = File("${rootDir}/gradle/init.gradle.kts")
+    includeBuild("networks")
     apply(from = initScriptFile.absolutePath)
+    settings.gradle.startParameter.initScripts = settings.gradle.startParameter.initScripts + initScriptFile
 }
 include(":imageloader")
 
