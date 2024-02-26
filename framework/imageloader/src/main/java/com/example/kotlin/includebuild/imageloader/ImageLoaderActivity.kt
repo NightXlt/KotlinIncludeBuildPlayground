58a42d0a9cb4859b8f102ef13b5dea7ca0815e5d
night <nightfeng26@gmail.com>
2024-02-26 20:38:06 +0800

    reproduce resource issue

---
+++ framework/imageloader/src/main/java/com/example/kotlin/includebuild/imageloader/ImageLoaderActivity.kt
@@ -0,0 +1,9 @@
+package com.example.kotlin.includebuild.imageloader
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @Author: 小灵通
+ * @Date: 2024.02.22
+ **/
+class ImageLoaderActivity : AppCompatActivity(R.layout.activity_imageloader)
